8714d33b288580419edde567fa51a1888b9b4175
kalmas <kylealmas@gmail.com>
2014-01-12 22:49:29 -0500

    fetches blog post dynamically

--- app/scripts/controllers/blog.js
+++ app/scripts/controllers/blog.js
@@ -4,4 +4,18 @@ angular.module('kalmasNetApp.controllers', [])
     .controller('BlogCtrl', ['$scope', '$routeParams', '$location', 'BlogPosts'
               , function ($scope, $routeParams, $location, BlogPosts) {
 
+    var slug = $routeParams.slug;
+    if(slug === undefined){
+      slug = 'scramble-squares';
+    }
+
+    BlogPosts.getPost(slug).then(function(post) {
+      $scope.title = post.title;
+      $scope.date = post.date;
+      $scope.desk = post.desk;
+      $scope.contentPath = post.contentPath;
+    });
+    
+
+
 }]);
--- app/scripts/services.js
+++ app/scripts/services.js
@@ -1,8 +1,24 @@
 'use strict';
 
 angular.module('kalmasNetApp.services', [])
-    .factory("BlogPosts", ['$resource', function($resource) {
+    .factory("BlogPosts", ['$http', '$q', function($http, $q) {
+  var path = 'content/'
+      , metaFormat = '.json'
+      , contentFormat = '.html';
 
-    return $resource;
+  return {
+    getPost: function(slug) {
+      var deferred = $q.defer()
+        , metaPath = path + slug + metaFormat
+        , contentPath = path + slug + contentFormat;
+      $http.get(metaPath).then(function(response) {
+        response.data.contentPath = contentPath;
+        deferred.resolve(response.data);
+      });
+      return deferred.promise;
+    }
+  };
     
 }]);
+
+
