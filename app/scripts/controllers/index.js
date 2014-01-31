a6e5db0e01b6eb323aa24c75552393d0da85fa15
kalmas <kylealmas@gmail.com>
2014-01-30 20:23:17 -0500

    adds bio to about

--- app/scripts/controllers/index.js
+++ app/scripts/controllers/index.js
@@ -4,6 +4,7 @@ angular.module('kalmasNetApp.controllers')
     .controller('IndexCtrl', ['$scope', 'About', function ($scope, About) {
 
   About.getAbout().then(function(about) {
+  	$scope.bio = about.bio;
   	$scope.skills = about.skills;
   	$scope.projects = about.projects;
   });
