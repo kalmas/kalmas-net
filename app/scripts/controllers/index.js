107a3efc169b223d9150ce8717c29f114470fe6e
kalmas <kylealmas@gmail.com>
2014-02-04 21:43:56 -0500

    implement biography

--- app/scripts/controllers/index.js
+++ app/scripts/controllers/index.js
@@ -8,6 +8,10 @@ angular.module('kalmasNetApp.controllers')
       $scope.bio = about.bio;
       $scope.skills = about.skills;
       $scope.projects = about.projects;
+      $scope.code = about.interests.code;
+      $scope.book = about.interests.book;
+      $scope.music = about.interests.music;
+      $scope.tv = about.interests.tv;
     });
 
   }]);
