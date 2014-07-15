8eb9f4e64af40a9cdfa8a90550798634baaab26d
kalmas <kylealmas@gmail.com>
2014-07-14 21:37:10 -0400

    fixes bare-bones karma test

---
+++ test/spec/controllers/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+describe('Controller: IndexCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('kalmasNetApp'));
+
+  var IndexCtrl,
+    scope,
+    $httpBackend;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
+    $httpBackend = _$httpBackend_;
+    $httpBackend.expectGET('blog/content/about.json').respond({interests: {}});
+    scope = $rootScope.$new();
+    IndexCtrl = $controller('IndexCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should attach bio', function () {
+    expect(scope.bio).toBeUndefined();
+    $httpBackend.flush();
+    expect(scope.bio).toBeUndefined();
+  });
+});
